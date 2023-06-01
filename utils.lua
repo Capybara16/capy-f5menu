4a076e00d0c246a8418a82c114cdba81a3390350
Capybara <105544563+Capybara16@users.noreply.github.com>
2023-06-01 15:27:27 +0200

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,13 +1,13 @@
-capy = {}
+Txpac = {}
 
-function capy.PlayAnim(dict, anim, wait)
+function Txpac.PlayAnim(dict, anim, wait)
     capy.LoadAnimDict(dict)
     TaskPlayAnim(PlayerPedId(), dict, anim, 8.0, 1.0, -1, 49, 0, 0, 0, 0 )
     Citizen.Wait(wait)
     ClearPedSecondaryTask(PlayerPedId()) 
 end
 
-function capy.LoadAnimDict(dict)
+function Txpac.LoadAnimDict(dict)
     while (not HasAnimDictLoaded(dict)) do
         RequestAnimDict(dict)
         Citizen.Wait(5)
